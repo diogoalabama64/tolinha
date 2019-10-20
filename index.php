5f31b45c7849e4d0777bea7f72a4b97b938d83b7
diogoalabama64 <56780713+diogoalabama64@users.noreply.github.com>
2019-10-20 01:26:08 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+<!DOCTYPE html>
+<html class="no-js" lang="zxx"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<title>Tolinha</title>
+		
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="The Heart - Creative Wedding Template by pixiefy">
+		<meta name="author" content="pixiefy">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css.css" rel="stylesheet">
+
+
+		<!-- All Style Sheet Start -->
+		<link rel="stylesheet" href="font-awesome.css">
+		<link rel="stylesheet" href="bootstrap.css">
+
+		<!-- All Style Sheet End -->
+
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="responsive.css">
+
+	</head>
+
+	<body class="the-heart-default-body the-heart-overly">
+	
+	<!-- The Heart Body Start -->
+
+	<!-- Preloder -->
+	<div id="prelaoder" style="display: none;"></div>
+
+	<!-- The Heart Welcome Section -->
+	<section class="the-heart-welcome-section jarallax black-overly" style="background-image: none;" data-jarallax-original-styles="background-image: url(img/love-bg3.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="the-heart-welcome-tbl">
+					<div class="the-heart-welcome-tbl-c">
+						<div class="wow fadeInUp" data-wow-duration="2s">
+							<div class="the-heart-welcome-content">
+								<h1>Tolinha</h1>
+							<span class="tiny-heart" style="top: 53%; left: 92%; width: 11.2px; height: 11.2px ; animation-delay: -0s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 50%; left: 21%; width: 7.2px; height: 7.2px ; animation-delay: -3s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 54%; left: 2%; width: 8.5px; height: 8.5px ; animation-delay: -2s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 62%; left: 88%; width: 6.7px; height: 6.7px ; animation-delay: -0s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 72%; left: 20%; width: 11.8px; height: 11.8px ; animation-delay: -1s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 52%; left: 53%; width: 11.8px; height: 11.8px ; animation-delay: -3s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 42%; left: 68%; width: 7.2px; height: 7.2px ; animation-delay: -3s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 43%; left: 52%; width: 6.4px; height: 6.4px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 53%; left: 66%; width: 11.1px; height: 11.1px ; animation-delay: -0s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 42%; left: 67%; width: 7.2px; height: 7.2px ; animation-delay: -3s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 49%; left: 42%; width: 6.8px; height: 6.8px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 55%; left: 9%; width: 9.3px; height: 9.3px ; animation-delay: -2s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 51%; left: 27%; width: 10.7px; height: 10.7px ; animation-delay: -2s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 54%; left: 17%; width: 6.5px; height: 6.5px ; animation-delay: -2s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 79%; left: 21%; width: 9px; height: 9px ; animation-delay: -1s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 61%; left: 100%; width: 7.5px; height: 7.5px ; animation-delay: -0s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 72%; left: 97%; width: 8.3px; height: 8.3px ; animation-delay: -3s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 75%; left: 100%; width: 8.9px; height: 8.9px ; animation-delay: -1s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 41%; left: 76%; width: 11.2px; height: 11.2px ; animation-delay: -1s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 77%; left: 75%; width: 7.4px; height: 7.4px ; animation-delay: -2s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 80%; left: 86%; width: 8.7px; height: 8.7px ; animation-delay: -1s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 66%; left: 32%; width: 8.3px; height: 8.3px ; animation-delay: -3s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 44%; left: 1%; width: 6.4px; height: 6.4px ; animation-delay: -1s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 67%; left: 61%; width: 10.1px; height: 10.1px ; animation-delay: -0s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 40%; left: 82%; width: 6.8px; height: 6.8px ; animation-delay: -1s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 66%; left: 81%; width: 9.4px; height: 9.4px ; animation-delay: -2s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 43%; left: 39%; width: 11.1px; height: 11.1px ; animation-delay: -3s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 76%; left: 81%; width: 8.2px; height: 8.2px ; animation-delay: -0s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 65%; left: 62%; width: 6.8px; height: 6.8px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 61%; left: 9%; width: 8.5px; height: 8.5px ; animation-delay: -1s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 61%; left: 86%; width: 10.7px; height: 10.7px ; animation-delay: -3s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 71%; left: 77%; width: 7.5px; height: 7.5px ; animation-delay: -1s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 80%; left: 84%; width: 9.7px; height: 9.7px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 53%; left: 50%; width: 8.9px; height: 8.9px ; animation-delay: -2s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 73%; left: 3%; width: 9.9px; height: 9.9px ; animation-delay: -0s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 69%; left: 65%; width: 7px; height: 7px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 80%; left: 73%; width: 11.4px; height: 11.4px ; animation-delay: -1s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 68%; left: 83%; width: 11.8px; height: 11.8px ; animation-delay: -3s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 65%; left: 9%; width: 10.5px; height: 10.5px ; animation-delay: -0s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 46%; left: 23%; width: 10.7px; height: 10.7px ; animation-delay: -3s; animation-duration: 2s"></span><span class="tiny-heart" style="top: 55%; left: 44%; width: 11.4px; height: 11.4px ; animation-delay: -3s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 79%; left: 70%; width: 6.5px; height: 6.5px ; animation-delay: -0s; animation-duration: 5s"></span><span class="tiny-heart" style="top: 65%; left: 70%; width: 6.3px; height: 6.3px ; animation-delay: -0s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 78%; left: 3%; width: 8.1px; height: 8.1px ; animation-delay: -0s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 52%; left: 48%; width: 11.5px; height: 11.5px ; animation-delay: -1s; animation-duration: 3s"></span><span class="tiny-heart" style="top: 74%; left: 7%; width: 7.9px; height: 7.9px ; animation-delay: -0s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 79%; left: 58%; width: 11.2px; height: 11.2px ; animation-delay: -2s; animation-duration: 4s"></span><span class="tiny-heart" style="top: 62%; left: 87%; width: 10.3px; height: 10.3px ; animation-delay: -0s; animation-duration: 3s"></span></div> <!-- .the-heart-welcome-content END -->
+							<h4 class="single-text">Me da um sorriso? :)</h4>
+							<div class="the-heart-btn-holder">
+							</div> <!-- .the-heart-btn-holder END -->
+						</div>
+					</div> <!-- .the-heart-welcome-tbl-c END -->
+				</div> <!-- .the-heart-welcome-tbl END -->
+			</div>
+		</div>
+	<div style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; z-index: -100;" id="jarallax-container-0"><div style="background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url(&quot;love-bg3.jpg&quot;); position: absolute; top: 0px; left: 0px; width: 1241px; height: 759px; overflow: hidden; pointer-events: none; margin-top: 79.5px; transform: translateY(-79.5px) translateZ(0px);"></div></div></section> <!-- .the-heart-welcome-floral-bg END -->
+
+
+	<!-- Js File Start -->
+
+	<script type="text/javascript" src="jquery-3.js"></script>
+	<script type="text/javascript" src="jquery.js"></script>
+	<script src="canvas-v9.js"></script>
+
+		<!-- header jarallax -->
+
+	<script src="jarallax.js"></script>
+	<script src="jarallax-element.js"></script>
+
+	<script type="text/javascript" src="blazy.js"></script>
+	<script src="main.js"></script>
+
+	
+</body></html>
+?>
